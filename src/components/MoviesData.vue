a5165c3321115b8260942ade252b95efac8a164e
saurav singh garia <91400696+Sauravsinghgaria@users.noreply.github.com>
2023-01-10 00:56:07 +0530

    re

---
+++ src/components/MoviesData.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="all">
+      <div>
+        <input type="text" placeholder="Enter Your Search" class="inp">
+        <button class="btn"> Clear Search</button>
+      </div>
+      <div class="cards">
+        <img src="" alt="image1">
+        <h2>Titles</h2>
+        <p>Released: Date</p>
+        <button type="button" class="btn2">Get More Info</button>
+      </div>
+      <div class="cards">
+        <img src="" alt="image1">
+        <h2>Titles</h2>
+        <p>Released: Date</p>
+        <button type="button" class="btn2">Get More Info</button>
+      </div>
+      <div class="cards">
+        <img src="" alt="image1">
+        <h2>Titles</h2>
+        <p>Released: Date</p>
+        <button type="button" class="btn2">Get More Info</button>
+      </div>
+      <div class="cards">
+        <img src="" alt="image1">
+        <h2>Titles</h2>
+        <p>Released: Date</p>
+        <button type="button" class="btn2">Get More Info</button>
+      </div>
+      <div class="cards">
+        <img src="" alt="image1">
+        <h2>Titles</h2>
+        <p>Released: Date</p>
+        <button type="button" class="btn2">Get More Info</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MoviesData',
+
+}
+</script>
+<style>
+.cards{
+  margin-left: 6vw;
+  margin-top: 4vw;
+  display:inline-block;
+}
+.btn2{
+  color:#ffffff;
+  border: 1px solid orangered;
+  background-color: #212121;
+
+}
+.all{
+  background-color: #212121;
+}
+.btn{
+  background-color: orangered;
+  color: #fff;
+  border:1px solid orangered;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  padding:8px 12px;
+  cursor: grab;
+}
+.inp{
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  padding: 8px 15px;
+  color: orangered;
+  margin-left: 6vw;
+  margin-top: 2vw;
+  border: 1px solid orangered;
+}
+</style>
---
+++ src/components/MoviesHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="heading">
+    <div style="position: absolute;">
+      <div class="headclass" style="margin-top: 15vw;margin-left: 6vw;">
+        <p>My Movie App</p>
+        <h1>Now Streaming</h1>
+        <button> View Movies</button>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'MoviesHeader',
+
+}
+</script>
+<style scoped>
+*{
+  font-family: Gugi,Cursive,Arial;
+}
+.heading{
+  background-image: url("../assets/movieHero.jpg");
+
+  width:100%;
+  height:27vw;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+h1{
+  color:#fff;
+  font-weight: bolder;
+}
+.headclass button{
+  background-color: orangered;
+  color: #fff;
+  border:1px solid orangered;
+  border-radius: 8px;
+  padding:8px 12px;
+}
+
+p{
+  color: orangered;
+  margin:0px;
+}
+</style>
