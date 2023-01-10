8db4bb7c1d4a5844c512cb16f7a9c5532785f3bd
saurav singh garia <91400696+Sauravsinghgaria@users.noreply.github.com>
2023-01-10 18:56:12 +0530

    ref

--- src/components/MoviesData.vue
+++ src/components/MoviesData.vue
@@ -4,7 +4,10 @@
         <input type="text" placeholder="Enter Your Search" v-model="querystr" class="inp">
         <button class="btn" @click="search" > Clear Search</button>
       </div>
-      <div class="cards" v-for="(res,index) in movname.results" :key="index">
+      <div v-if="movname.length == 0">
+        <h1 style="color:white; margin-left:6vw;">No Matches</h1>
+      </div>
+      <div class="cards" v-else v-for="(res,index) in movname" :key="index">
         <div>
           <span class="xyz">{{res.vote_average}}</span>
           <img :src="`https://image.tmdb.org/t/p/w500/${res.poster_path}`" class="images" alt="errorimage">
@@ -12,12 +15,11 @@
           <p>Released: {{res.release_date}}</p>
           <button type="button" class="btn btn2" @click="info">Get More Info</button>
         </div>
-
       </div>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: 'MoviesData',
   data(){
@@ -29,15 +31,18 @@ export default {
   mounted(){
     fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=bb5c9a25161603cb7d1205e55e4cbe88&language=en-US&page=1")
         .then((res)=>res.json())
-        .then(data => this.movname = data)
+        .then(data => this.movname = data.results)
         .catch(err => console.log(err))
   },
   methods:{
-    search(){
-      fetch("https://api.themoviedb.org/3/search/movie?api_key=bb5c9a25161603cb7d1205e55e4cbe88&query="+this.querystr+"\"")
+    async search(){
+      await fetch("https://api.themoviedb.org/3/search/movie?api_key=bb5c9a25161603cb7d1205e55e4cbe88&query="+this.querystr+"\"")
           .then((res)=>res.json())
-          .then(data => this.movname = data)
+          .then(data => {
+            this.movname = data.results.filter((x) => x?.poster_path)
+          })
           .catch(err => console.log(err))
+      this.querystr = ""
     },
     info(){
 
